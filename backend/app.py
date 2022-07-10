a99b536401d55d3b2f84ebc2c76331c77f71b40f
Luke <2342915h@student.gla.ac.uk>
2022-07-10 15:22:25 +0100

    Updated the API to validate the score value to make sure it is at least £100

--- backend/app.py
+++ backend/app.py
@@ -27,27 +27,36 @@ def scores():
         data = request.get_data(as_text=True)
         data = ast.literal_eval(data)
 
+        if data.get('value') is None:
+            return 'Invalid format - missing "value"', 422
+        try:
+            float(data.get('value'))
+        except ValueError:
+            return 'Value needs to be a number'
+        if float(data.get('value')) < 100:
+            return 'Invalid score - needs to be at least £100.00', 422
+
         if data.get('id') is None:
-            return 'Invalid format - missing "ID"'
+            return 'Invalid format - missing "ID"', 422
 
         if data.get('name') is None:
-            return 'Invalid format - missing "name"'
+            return 'Invalid format - missing "name"', 422
         if len(data.get('name')) < 2:
-            return 'Name is too short'
+            return 'Name is too short', 422
 
         if data.get('trades') is None:
-            return 'Invalid format - missing "trades"'
+            return 'Invalid format - missing "trades"', 422
         try:
             int(data.get('trades'))
         except ValueError:
-            return 'Trades needs to be a number'
+            return 'Trades needs to be a number', 422
 
         if data.get('time') is None:
-            return 'Invalid format - missing "time"'
+            return 'Invalid format - missing "time"', 422
         try:
             int(data.get('time'))
         except ValueError:
-            return 'Time needs to be a number (seconds)'
+            return 'Time needs to be a number (seconds)', 422
 
         score = {
             'id': data.get('id'),
@@ -61,11 +70,11 @@ def scores():
             try:
                 scores = json.load(f)
             except json.decoder.JSONDecodeError:
-                print('Score file is empty.')
+                pass
         scores.append(score)
         with open('./scores.json', 'w') as f:
             json.dump(scores, f)
 
-        return 'Score submitted!'
+        return 'Score submitted!', 200
 
-    return 'Something went wrong.'
+    return 'Something went wrong.', 500
